bf3684bc43159dadce076b3ab87a177ef32b6557
andrey.romanyuk <andrey.romanyuk@dif.tech>
2023-01-08 17:28:04 +0300

    HSE-1: bump kotlin version to 1.7.21, compose to 1.4.0 alpha

--- android/sources/app/build.gradle.kts
+++ android/sources/app/build.gradle.kts
@@ -4,5 +4,6 @@ android { namespace = "goshka133.football.app" }
 
 dependencies {
   implementation(libs.compose.activity)
+  implementation(libs.bundles.compose)
   implementation(libs.coroutines)
 }
